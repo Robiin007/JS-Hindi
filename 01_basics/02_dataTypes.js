01824a890b5ecd36eb0bb499a80313faeaa038e5
Robiin007 <129576912+Robiin007@users.noreply.github.com>
2024-01-26 08:23:47 +0000

    js lectures

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+console.log(3 
+    +
+    3);
+
+    console.log("Robin");
+
+    let name = "Robin";
+    let age = 18;
+    let isLoggedIn = false;
+    let state;
+
+    // number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
